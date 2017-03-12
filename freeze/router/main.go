60bbb3076186e2801c23c9d984c85a966765692f
hjianhao <hjianhao@hotmail.com>
2017-03-12 22:55:26 +0800

    add time cost

--- freeze/router/main.go
+++ freeze/router/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "time"
 
     "github.com/crosbymichael/cgroups"
 )
@@ -10,6 +11,7 @@ import (
 func pause(w http.ResponseWriter, r *http.Request) {
     id := r.Header.Get("container_id")
     fmt.Println("Pause function, container id : " + id)
+    start := time.Now ()
     if control, err := cgroups.Load(cgroups.V1, cgroups.StaticPath("/docker/" + id)); err != nil {
         fmt.Fprintf(w, err.Error())
         return
@@ -17,7 +19,8 @@ func pause(w http.ResponseWriter, r *http.Request) {
         if err = control.Freeze(); err != nil {
             fmt.Fprintf(w, err.Error())
         } else {
-            fmt.Fprintf(w, "Container %s paused", id)
+            cost := time.Now ().Sub(start).Nanoseconds() / 1000000
+            fmt.Fprintf(w, "Container %s paused, cost : %d ms\n", id, cost)
         }
     }
 }
@@ -25,6 +28,7 @@ func pause(w http.ResponseWriter, r *http.Request) {
 func resume(w http.ResponseWriter, r *http.Request) {
     id := r.Header.Get("container_id")
     fmt.Println("Resume function, container id : %s" + id)
+    start := time.Now ()
     if control, err := cgroups.Load(cgroups.V1, cgroups.StaticPath("/docker/" + id)); err != nil {
         fmt.Fprintf(w, err.Error())
         return
@@ -32,7 +36,8 @@ func resume(w http.ResponseWriter, r *http.Request) {
         if err = control.Thaw(); err != nil {
             fmt.Fprintf(w, err.Error())
         } else {
-            fmt.Fprintf(w, "Container %s resumed", id)
+            cost := time.Now ().Sub(start).Nanoseconds() / 1000000
+            fmt.Fprintf(w, "Container %s resumed, cost : %d ms\n", id, cost)
         }
     }
 }
